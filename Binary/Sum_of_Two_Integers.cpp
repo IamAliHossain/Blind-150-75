3f82680259d9ad0739f39e983f2d80af07c050bc
IamAliHossain <alihossain.cse.pstu@gmail.com>
2025-04-26 12:11:50 +0600

    Bit Manipulation folder Named Binary Added

---
+++ Binary/Sum_of_Two_Integers.cpp
@@ -0,0 +1,66 @@
+/*
+    Problem Link: https://leetcode.com/problems/sum-of-two-integers/description/
+    Problem Name: Sum of Two Integers
+    Difficulty  : Medium
+    Time Complexity : O()
+    Space Complexity: O()
+
+*/
+/*
+====================================================================================================
+
+Given two integers a and b, return the sum of the two integers without using the operators + and -.
+
+Example 1:
+
+Input: a = 1, b = 2
+Output: 3
+Example 2:
+
+Input: a = 2, b = 3
+Output: 5
+ 
+
+Constraints:
+
+-1000 <= a, b <= 1000
+
+==================================================================================================
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+    Using Bit Manipulation Technique 
+*/
+
+class Solution {
+public:
+    int getSum(int a, int b) {
+        int sum ;
+        int carry;
+        while(carry){
+            sum = a ^ b;
+            carry = (a & b) << 1;
+            a = sum;
+            b = carry ;
+        }
+        return sum ;
+    }
+};
+
+int main(){
+    int a = 20 , b = 30 ;
+    int sum ;
+    int carry ;
+    int add_sum;
+    while(carry){
+        sum = a ^ b;
+        carry = (a & b) << 1;
+        a = sum;
+        b = carry ;
+    }
+    cout << sum  << endl;
+}

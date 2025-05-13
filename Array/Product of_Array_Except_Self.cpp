62089fb0b73afefc31d6ff2bb4127fa0995214e6
IamAliHossain <alihossain.cse.pstu@gmail.com>
2025-05-13 17:33:25 +0600

    Interesting is used here

--- Array/Product of_Array_Except_Self.cpp
+++ Array/Product of_Array_Except_Self.cpp
@@ -9,7 +9,7 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-// Solution using Divison operation
+// Solution using Divison operation go below for best solution
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
@@ -36,6 +36,35 @@ public:
             for(int i=0; i<nums.size(); i++){
                 ans.push_back(product/nums[i]);
             }
+        }
+        return ans;
+    }
+};
+
+/*
+    The beutiful technique but too simple to think The logic behind the scene is 
+    ith index contain the multiplicaiton of [0, i-1] indices and 
+    [i+1, n-1] indices that means for any a[i] = a0 * a1 * a[i-1] * a[i+1] * a[i+2] ...* a[n-1] 
+
+*/
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> left(n), right(n); 
+       
+        left[0] = 1, right[n-1] = 1;
+        
+        for(int i=1; i<n; i++){
+            left[i] = left[i-1] * nums[i-1];
+        }
+
+        for(int i=n-2; i>=0; i--){
+            right[i] = right[i+1] * nums[i+1];
+        }
+        vector<int> ans(n);
+        for(int i=0; i<n; i++){
+            ans[i] = left[i] * right[i];
         }
         return ans;
     }

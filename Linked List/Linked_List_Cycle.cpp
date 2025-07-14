9531523253d1d7dfc82fe98d00d9a7401f35da25
IamAliHossain <alihossain.cse.pstu@gmail.com>
2025-07-14 17:30:35 +0600

    solved using Floyd Warshal Algorithm

--- Linked List/Linked_List_Cycle.cpp
+++ Linked List/Linked_List_Cycle.cpp
@@ -4,7 +4,7 @@
     Problem Diff: Easy
 
 */
- /* solution using Floyd's Cycle Finding Algorithm 
+ /* solution using Floyd Warshall Cycle Finding Algorithm 
     Follow the steps below to solve the problem:
 
     1. Traverse linked list using two pointers.
@@ -12,23 +12,35 @@
     3. If these pointers meet at the same node then there is a loop. 
         If pointers do not meet then the linked list doesn’t have a loop.
  */
+
+ #include<bits/stdc++.h>
+ using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+
+// This is solved by using hare and tortoise algorithm, also known as Floyd Warshall algo
+// TC : O(N)
+// SC : O(1)
 class Solution {
     public:
     bool hasCycle(ListNode *head) {
         
-        ListNode* slow_pointer = head;
-        ListNode* fast_pointer = head;
-        
-        while(1){
-            if(slow_pointer->next == NULL){
-                return false;
-            }
-            if(slow_pointer == fast_pointer){
+        ListNode* oneStep = head;
+        ListNode* twoStep = head;
+
+        while(oneStep != NULL && twoStep != NULL && twoStep->next != NULL){
+            oneStep = oneStep->next;
+            twoStep = twoStep->next->next;
+            // if they meet, there is a cycle
+            if(oneStep == twoStep){
                 return true;
             }
-            
-            slow_pointer = slow_pointer->next;
-            fast_pointer = fast_pointer->next->next;
         }
         return false;
     }

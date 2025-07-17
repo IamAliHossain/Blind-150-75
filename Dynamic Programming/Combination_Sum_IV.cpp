3d8bf10fc4bfe34c69f8fe1b75d5f8707d3f693a
IamAliHossain <alihossain.cse.pstu@gmail.com>
2025-07-17 13:13:41 +0600

    combination sum4 all 3 approach added

--- Dynamic Programming/Combination_Sum_IV.cpp
+++ Dynamic Programming/Combination_Sum_IV.cpp
@@ -13,6 +13,94 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/*
+    This recursive solution leads TLE
+    N = nums.size()
+    T = target
+    TC : O(N * T)
+    SC : O(N * T)
+    Now to solve TLE problem use memoization technique
+
+    dp array এর dimension নির্ভর করে হচ্ছে function কয়টা arguments এর উপর নির্ভর করছে তার উপর
+    এখানে index & target এই 2 টার উপর নির্ভর করে তাই 2D DP হবে। index can be N ও target তাই 
+    N * Target। 
+    অতএব dp[N][Target+1]
+
+*/
+
+/*-------------------------------Recursive Solution Ends---------------------------------------------*/
+
+
+
+/*-------------------------------Memoizstion Solution Starts---------------------------------------------
+    This recursive solution leads TLE
+    N = nums.size()
+    T = target
+    TC : O(N * T)
+    SC : O(N * T)
+    Now to solve TLE problem use memoization technique
+
+    dp array এর dimension নির্ভর করে হচ্ছে function কয়টা arguments এর উপর নির্ভর করছে তার উপর
+    এখানে index & target এই 2 টার উপর নির্ভর করে তাই 2D DP হবে। index can be N ও target তাই 
+    N * Target। 
+    অতএব dp[N][Target+1]
+
+*/
+class Solution {
+public:
+    long long ways(int index, int target, vector<int>& nums, vector<vector<int>>& dp){
+        
+        if(target < 0) return 0;
+        if(target == 0) return 1;
+        if(dp[index][target] != -1) return dp[index][target];
+        int ans = 0;
+        // This for loop is used instead of take/notTake technique because this take
+        //different sequences are counted as different combinations 4 = 1+3 and 4 = 3+1 are two different combinations
+        for(int i=0; i<nums.size(); i++){
+            ans += ways(i, target-nums[i], nums, dp);
+        }
+        
+        return dp[index][target] = ans;
+
+    }
+    int combinationSum4(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(target+1, -1));
+        return ways(n-1, target, nums, dp);
+    }
+};
+
+// memoization solution2
+
+class Solution {
+public:
+    long long ways(int index, int target, vector<int>& nums, vector<vector<int>>& dp){
+        
+        if(target < 0) return 0;
+        if(target == 0) return 1;
+        if(dp[index][target] != -1) return dp[index][target];
+        int ans = 0;
+        // This for loop is used instead of take/notTake technique because this take
+        //different sequences are counted as different combinations 4 = 1+3 and 4 = 3+1 are two different combinations
+        for(int i=0; i<nums.size(); i++){
+            ans += ways(i, target-nums[i], nums, dp);
+        }
+        
+        return dp[index][target] = ans;
+
+    }
+    int combinationSum4(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(target+1, -1));
+        return ways(n-1, target, nums, dp);
+    }
+};
+/*-------------------------------Memoizstion Solution Ends---------------------------------------------*/
+
+
+
+
+/*-------------------------------Tabulation Solution Starts---------------------------------------------*/
 
 class Solution {
 public:

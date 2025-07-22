e103fecfb1877a3d11895541eac813209859cf35
IamAliHossain <alihossain.cse.pstu@gmail.com>
2025-07-22 09:35:31 +0600

    2 solution added in O(N) and O(N*logN)

--- Graph/Longest_Consecutive_Secuence.cpp
+++ Graph/Longest_Consecutive_Secuence.cpp
@@ -2,11 +2,13 @@
     Problem Link : https://leetcode.com/problems/longest-consecutive-sequence/description/
     Problem Name : 128. Longest Consecutive Sequence
     Difficulty   : Medium
-    Time Complexity : O(N)
-    Space Complexity : O(N)
+    Time Complexity : 
+    Space Complexity : 
     ================================================================================================
 
-    Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+    Given an unsorted array of integers nums, return the length of the longest 
+    consecutive elements sequence.
+
     You must write an algorithm that runs in O(n) time.
 
     Example 1:
@@ -34,25 +36,75 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-class Solution{
-    public:
-    int longestConsecutive(vector<int> &nums) {
-        
-        set<int> se;
-        for(int i=0; i<nums.size(); i++){
-            se.insert(nums[i]);
+
+//Most Optimal solution
+/*
+    using hash set follow up O(N) time complexity is achieved now
+    TC : O(N) + O(N+N) --> O(3N) : O(N)
+    SC : O(N)
+*/
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> hash;
+        int n = nums.size();
+        for(int i=0; i<n; i++){ // O(N)
+            hash.insert(nums[i]); // unsorted sorted hash set takes O(1) for per insert
         }
-        int cnt = 1, mxCnt = 1;
-        for(auto u : se){
-            if(se.find(u-1) == se.end()){
-                cnt = 1;
-                while(se.find(u+1) != se.end()){
+        
+        int mxCnt = 1, left = 0, right = 1;
+        for(auto x: hash){ // O(N+N)
+            if(hash.find(x - 1) == hash.end()){
+                int cnt = 1 ;
+                while(hash.find(x + 1) != hash.end()){
                     cnt++;
-                    u++;
+                    x = x+1;
                 }
                 mxCnt = max(mxCnt, cnt);
-            } 
+            }
         }
+        if(!n)return 0;
+        return mxCnt;
+    }
+};
+
+// solution 2
+/*
+    sliding window
+    TC : O(n*logn)
+    SC : O(N)
+
+    but follow up korte bola hoyeche O(N) TC te
+
+*/
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        map<int, int> hash;
+        int n = nums.size();
+        for(int i=0; i<n; i++){
+            hash[nums[i]]++; // sorted map takes O(logn) for per insert
+        }
+        vector<int> v;
+        for(auto [x, y] : hash){
+            v.push_back(x);
+        }
+        int mxCnt = 1, left = 0, right = 1;
+        n = v.size();
+        while(right<n){
+            if(v[right-1] == v[right]-1){
+                right++;
+                mxCnt = max(mxCnt, right - left);
+            }
+            else{
+                
+                left = right;
+                right++;
+            }
+        }
+        if(!n)return 0;
         return mxCnt;
     }
 };

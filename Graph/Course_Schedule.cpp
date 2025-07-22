a9aa786a233b11c5c68e2b1484154366887cf9e9
IamAliHossain <alihossain.cse.pstu@gmail.com>
2025-07-22 07:50:55 +0600

    cycle finding in a directed graph

--- Graph/Course_Schedule.cpp
+++ Graph/Course_Schedule.cpp
@@ -36,9 +36,67 @@
 
 */
 
+// solution 1
+
+/*
+    This problem indicates finding cycles in a directed graph since we need to take
+    course and that course has a prerequisite course also, so need to check that if 
+    there is cycle or not, if not then we can finish all the courses, other wise because of
+    cycle we can't finish all the courses
+
+    1->2 2->3 3->1 : this will make a directed graph which has a cycle, so in this case 
+    we can't finish all the courses
+    
+
+*/
+
+class Solution {
+public:
+    // dfs technique to detect cycle have or not in a directed graph
+    bool isCycle(int vertex, vector<vector<int>> &adj, vector<bool> &visited, vector<bool> &pathVisited){
+        visited[vertex] = true;
+        pathVisited[vertex] = true;
+
+        for(auto child : adj[vertex]){
+            if(!visited[child]){
+                bool cycleFound = isCycle(child, adj, visited, pathVisited);
+                if(cycleFound)return true;
+            }
+            else{
+                if(pathVisited[child])return true;
+            }
+            
+        }
+        pathVisited[vertex] = false;
+        return false;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int n = numCourses;
+
+        vector<vector<int>> adj(n);
+        // since there is prerequisite so this is a directed graph
+        for(auto it : prerequisites){
+            adj[it[1]].push_back(it[0]);
+        }
+        vector<bool> visited(n, false), pathVisited(n, false);
+        for(int i=0; i<numCourses; i++){
+            if(!visited[i]){
+                bool cycleFound = isCycle(i, adj, visited, pathVisited);
+                // when cycle is found we can't finish all the courses so return false
+                if(cycleFound){
+                    return false;
+                }
+            }
+        }
+        return true;
+
+    }
+};
+
+// solution 2
 /*
 
-    Solution Technique:
+    Solution Technique BFS using topological sort idea:
     Breaking down the intuition step by step:
 
     We construct a directed graph where each node is a course, and edges represent prerequisites (a directed edge from course b to course a means b is a prerequisite for a).

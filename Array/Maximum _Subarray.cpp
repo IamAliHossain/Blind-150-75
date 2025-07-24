ec93e4cfda9d9ef44929d8a3b1ecd729d3c218b2
IamAliHossain <alihossain.cse.pstu@gmail.com>
2025-07-24 09:46:31 +0600

    solved by myself

--- Array/Maximum _Subarray.cpp
+++ Array/Maximum _Subarray.cpp
@@ -8,7 +8,31 @@
 
 #include<bits/stdc++.h>
 using namespace std;
+/* 
+    There added two same solution using kadan's algorithm 
+    one is my own second one is other's 
 
+    TC : O(N)
+    SC : O(1)
+*/
+// solution 1 : my Own 
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = nums[0];
+        int n = nums.size();
+        int sum = 0;
+        for(int i=0; i<n; i++){
+            sum += nums[i];
+            maxSum = max(maxSum, sum);
+            if(sum < 0){ 
+                sum = 0;
+            }
+        }
+        return maxSum;
+    }
+};
+// solution 2
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {

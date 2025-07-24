269dc2fac32fc43a66f53a4101b64329960c9db6
IamAliHossain <alihossain.cse.pstu@gmail.com>
2025-07-24 09:42:18 +0600

    Solved by me

--- Array/Product of_Array_Except_Self.cpp
+++ Array/Product of_Array_Except_Self.cpp
@@ -5,11 +5,44 @@
     Time Complexity : O(N)
     Space Complexity: O()
 
+    
+    Given an integer array nums, return an array answer such that answer[i] is equal to 
+    the product of all the elements of nums except nums[i].
+
+    The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+    You must write an algorithm that runs in O(n) time and without using the division operation.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+Example 2:
+
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+ 
+
+Constraints:
+
+2 <= nums.length <= 10^5
+-30 <= nums[i] <= 30
+The input is generated such that answer[i] is guaranteed to fit in a 32-bit integer.
+ 
+
+Follow up: Can you solve the problem in O(1) extra space complexity? (The output array 
+does not count as extra space for space complexity analysis.)
+
 */
 
 #include<bits/stdc++.h>
 using namespace std;
-// Solution using Divison operation go below for best solution
+/* 
+    Solution using Divison operation but it problem said to not use division operation
+    that's why go below for best solution
+*/
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
@@ -41,31 +74,49 @@ public:
     }
 };
 
-/*
-    The beutiful technique but too simple to think The logic behind the scene is 
-    ith index contain the multiplicaiton of [0, i-1] indices and 
-    [i+1, n-1] indices that means for any a[i] = a0 * a1 * a[i-1] * a[i+1] * a[i+2] ...* a[n-1] 
 
+
+/*
+    This solution is mine after 45 minutes of brainstorming : 
+
+    যেই index এ আছি সেই index এর আগের prefix product & তার পরের index এর suffix product 
+    multiply করে ঐ index এর product পাবো except that index, এজন্য we need a prefix product array 
+    যেটা index er left part er product bear করবে & একটা suffix product array লাগবে 
+    যেটা কোন index এর right part এর suffix product bear করবে ।
+
+    কোন index এর product except that index = prefProduct[index-1] * suffixProduct[inex+1]
+ 
+    TC : O(N)
+    SC : O(N) + O(N) : O(N)
+
+    Follow Up task for Space Complexity is O(!) where I used O(N) that's why I need to 
+    optimize that which added in the next solution 3
 */
+
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int> left(n), right(n); 
-       
-        left[0] = 1, right[n-1] = 1;
-        
+        vector<int> prefProd(n), suffProd(n);
+        prefProd[0] = nums[0];
+        suffProd[n-1] = nums[n-1];
         for(int i=1; i<n; i++){
-            left[i] = left[i-1] * nums[i-1];
+            prefProd[i] = prefProd[i-1] * nums[i];
         }
-
         for(int i=n-2; i>=0; i--){
-            right[i] = right[i+1] * nums[i+1];
+            suffProd[i] = suffProd[i+1] * nums[i];
         }
-        vector<int> ans(n);
         for(int i=0; i<n; i++){
-            ans[i] = left[i] * right[i];
+            if(i == 0){
+                nums[i] = suffProd[i+1];
+            }
+            else if(i == n-1){
+                nums[i] = prefProd[i-1];
+            }
+            else{
+                nums[i] = prefProd[i-1] * suffProd[i+1];
+            }
         }
-        return ans;
+        return nums;
     }
 };
